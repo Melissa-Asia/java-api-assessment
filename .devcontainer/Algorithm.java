37a2a6b519399abd4072082b57245bc169992188
Melissa <Asiamah.m98@gmail.com>
2024-01-18 12:40:05 +0000

    Create a Binary search Algorithm for string inputs

---
+++ .devcontainer/Algorithm.java
@@ -0,0 +1,21 @@
+
+/*Create a Binary Search algorithm that searchs the data for a hairstyle based on the users input/search */
+public static String BinarySearch(String [] arr, string target){
+ String left = "A";
+ String right = arr.length - 1;
+
+ while (left <=right) {
+    String mid = left + (right-left)/2;
+    if (arr[mid] == target) {
+        return mid;
+    }
+
+    if (arr[mid] < target) {
+        left = mid + 1;
+    }else{
+        right = mid -1;
+    }
+ }
+ return -1
+}
+    
