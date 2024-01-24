d448e5a5333c484f09600f3193972c127e21984b
Melissa Asiamah <Asiamah.m98@gmail.com>
2024-01-24 13:14:21 +0000

    Make changes

---
+++ Salon API/Salon/Salon.java
@@ -0,0 +1,10 @@
+public class Salon {
+
+    public static void JSONFileHandler
+    {
+
+    public List<String> readFile(String Salon.json)
+
+    public void save(Employee employee, String outputFile)
+    }
+}
