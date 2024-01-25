1296e794d258d2e98ee355c516548eb193cb167e
Melissa Asiamah <Asiamah.m98@gmail.com>
2024-01-25 00:17:15 +0000

    Add Gson

--- src/main/java/com/cbfacademy/apiassessment/core/Salon copy.java
+++ src/main/java/com/cbfacademy/apiassessment/core/Salon copy.java
@@ -1,30 +1,24 @@
-package com.cbfacademy.apiassessment.core;
-
-import java.util.List;
-import java.util.HashMap;
-import java.io.*;
-import java.lang.reflect.Type;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-{public String Hairstyles(String ID,String Wash,String Blowdry,String treatment,String Assessment,String Style,int price){this.ID=ID;this.Wash=Wash;this.Blowdry=Blowdry;this.treatment=treatment;this.Assessment=Assessment;this.Style=Style;this.Price=price;};
-
-// Getters and Setters
-
-public HashMap<String,String>Hairstyles()throws FileNotFoundException{String path="com.cbfacademy.apiassessment.core.Data - Jsonfiles./Salon.json";BufferedReader bufferedReader=new BufferedReader(new FileReader(path));
-
-Gson gson=new Gson();HashMap<String,String>json=gson.fromJson(bufferedReader,HashMap.class);return json;}}
+/*
+ * package com.cbfacademy.apiassessment.core;
+ * 
+ * import java.util.List;
+ * import java.io.FileNotFoundException;
+ * import java.io.FileReader;
+ * import java.io.IOException;
+ * import java.io.JSONFileHandler;
+ * 
+ * public class JSONFileHandler {
+ * public List<String> readFile(String Salon.json);
+ * 
+ * public void save(Salon salon, String outputFile);
+ * }
+ * 
+ * 
+ * public HashMap<String,String>Hairstyles()throws FileNotFoundException{String
+ * path="com.cbfacademy.apiassessment.core.Data - Jsonfiles./Salon.json"
+ * ;BufferedReader bufferedReader=new BufferedReader(new FileReader(path));
+ * 
+ * Gson gson=new
+ * Gson();HashMap<String,String>json=gson.fromJson(bufferedReader,HashMap.class)
+ * ;return json;}}
+ */
--- src/main/java/com/cbfacademy/apiassessment/core/SalonDesk.java
+++ src/main/java/com/cbfacademy/apiassessment/core/SalonDesk.java
@@ -1,5 +1,29 @@
 package com.cbfacademy.apiassessment.core;
 
+import java.io.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.Gson;
+import java.io.FileNotFoundException;
+/* 
+import java.util.List;
+import java.util.*;
+import java.util.HashMap;
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+*/
+
 public class SalonDesk {
 
     public String ID;
@@ -10,7 +34,85 @@ public class SalonDesk {
     public String Style;
     public int Price;
 
-    public SalonDesk() {
+    public SalonDesk(String ID, String Wash, String Blowdry, String treatment, String Assessment, String Style,
+            int price) {
+        this.ID = ID;
+        this.Wash = Wash;
+        this.Blowdry = Blowdry;
+        this.treatment = treatment;
+        this.Assessment = Assessment;
+        this.Style = Style;
+        this.Price = price;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String iD) {
+        ID = iD;
+    }
+
+    public String getWash() {
+        return Wash;
+    }
+
+    public void setWash(String wash) {
+        Wash = wash;
+    }
+
+    public String getBlowdry() {
+        return Blowdry;
+    }
+
+    public void setBlowdry(String blowdry) {
+        Blowdry = blowdry;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getAssessment() {
+        return Assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        Assessment = assessment;
+    }
+
+    public String getStyle() {
+        return Style;
+    }
+
+    public void setStyle(String style) {
+        Style = style;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    private static final SalonDesk[] SALON_DATA() throws FileNotFoundException {
+        /*
+         * BufferedReader bufferedReader = new BufferedReader(new
+         * FileReader("com.cbfacademy.apiassessment.core.Data.Salon.json"));
+         */
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader("Salon.json"));
+        SalonDesk[] data = gson.fromJson(reader, SalonDesk[].class); // contains the whole Salon list
+
+        {
+            return data;
+        }
 
     }
 }
